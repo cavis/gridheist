bb0cdb9443a10290f953a9a9f64d2f33c900964e
cavis <ryancavis@gmail.com>
2013-09-13 21:11:38 -0600

    better-ify dynamic test page

---
+++ test/dynamic.js
@@ -0,0 +1,129 @@
+//
+// gridheist config
+//
+var defaults = {
+  thumbBorder:      10,
+  thumbMinHeight:   100,
+  thumbMaxHeight:   200,
+  expandHeight:     300,
+  expandSideWidth:  200,
+  // non-gridheist
+  layoutType: 'fluid',
+  layoutFluidValue: 100,
+  layoutFixedValue: 1000
+}
+var config = _.extend({}, defaults);
+
+// load any stored configs
+if (localStorage) {
+  _.each(config, function(val, key) {
+    if (localStorage.getItem(key)) {
+      config[key] = localStorage.getItem(key);
+      config[key] = parseInt(config[key]) || config[key];
+    }
+  });
+}
+
+// dom => config
+var updateConfig = function() {
+  config.layoutType = $('select[name="layoutType"]').val();
+  config.layoutFluidValue = parseInt( $('input[name="layoutFluidValue"]').val() )
+  config.layoutFixedValue = parseInt( $('input[name="layoutFixedValue"]').val() )
+
+  if (config.layoutType == 'fluid') {
+    $('#gallery').css({marginLeft: config.layoutFluidValue, marginRight: config.layoutFluidValue, width: 'auto'});
+  }
+  else {
+    $('#gallery').css({marginLeft: 'auto', marginRight: 'auto', width: config.layoutFixedValue});
+  }
+
+  config.thumbBorder    = parseInt( $('input[name="thumbBorder"]').val() );
+  config.thumbMinHeight = parseInt( $('input[name="thumbMinHeight"]').val() );
+  config.thumbMaxHeight = parseInt( $('input[name="thumbMaxHeight"]').val() );
+  $('#gallery').gridHeist('update', config);
+
+  // update storage
+  if (localStorage) {
+    _.each(config, function(val, key) {
+      localStorage.setItem(key, val);
+    });
+  }
+}
+
+// config => dom
+var updateDom = function() {
+  $('select[name="layoutType"]').val(config.layoutType);
+  $('input[name="layoutFluidValue"]').val(config.layoutFluidValue)
+    .filter('[data-slider]').simpleSlider('setValue', config.layoutFluidValue);
+  $('input[name="layoutFixedValue"]').val(config.layoutFixedValue)
+    .filter('[data-slider]').simpleSlider('setValue', config.layoutFixedValue);
+
+  if (config.layoutType == 'fluid') {
+    $('.layout-fluid').show() && $('.layout-fixed').hide();
+  }
+  else {
+    $('.layout-fluid').hide() && $('.layout-fixed').show();
+  }
+
+  $('input[name="thumbBorder"]').val(config.thumbBorder)
+    .filter('[data-slider]').simpleSlider('setValue', config.thumbBorder);
+  $('input[name="thumbMinHeight"]').val(config.thumbMinHeight)
+    .filter('[data-slider]').simpleSlider('setValue', config.thumbMinHeight);
+  $('input[name="thumbMaxHeight"]').val(config.thumbMaxHeight)
+    .filter('[data-slider]').simpleSlider('setValue', config.thumbMaxHeight);
+}
+
+// dom it!
+$(function() {
+  updateDom();
+
+  // insert images
+  $.getJSON('images.json', function(data) {
+    var $gallery = $('#gallery').html('');
+
+    if (data && data.hits && data.hits.length) {
+      for (var i=0; i<data.hits.length; i++) {
+        var w = data.hits[i].webformatWidth,
+            h = data.hits[i].webformatHeight,
+            sm = data.hits[i].webformatURL,
+            lg = data.hits[i].webformatURL;
+        $gallery.append('<a class="thumb" href="'+sm+'"><img data-width="'+w+'" data-height="'+h+'" src="'+lg+'"/></a>');
+      }
+
+      $('#gallery').gridHeist(config);
+    }
+  });
+
+  // sync sliders
+  $('input[data-slider]').on('slider:changed', function(e, data) {
+    $('[name="' + $(this).attr('name') + '"]').val(data.value);
+  });
+
+  // layout change
+  $('input').on('change', updateConfig);
+  $('select').on('change', function() {
+    $('input').off('change', updateConfig);
+    if ($(this).val() == 'fluid') {
+      $('.layout-fluid').show() && $('.layout-fixed').hide();
+      $('input[name="layoutFluidValue"]').val(100).filter('[data-slider]').simpleSlider('setValue', 100);
+    }
+    else {
+      $('.layout-fluid').hide() && $('.layout-fixed').show();
+      $('input[name="layoutFixedValue"]').val(1000).filter('[data-slider]').simpleSlider('setValue', 1000);
+    }
+    updateConfig();
+    $('input').on('change', updateConfig);
+  });
+
+  // revert to defaults
+  $('h2.reset').click(function() {
+    alert('Reset to defaults');
+    config = _.extend({}, defaults);
+    if (localStorage) localStorage.clear();
+    $('input').off('change', updateConfig);
+    updateDom();
+    $('input').on('change', updateConfig);
+    $('#gallery').gridHeist('update', config);
+  });
+
+});
